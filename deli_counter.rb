0dda328cd4ae1c065996f4536db5a9be47ddfd68
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-19 18:21:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(array)
   if array.length > 0
     queue = ""
     array.each_with_index do |customer, index|
-    queue << "#{index + 1}. #{customer} "
+    queue << " #{index + 1}. #{customer}"
     end
     puts "The line is currently: #{queue}"
   else
